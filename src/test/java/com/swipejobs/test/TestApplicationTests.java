1ae15ef047b99aed308fd4bf06a79ee7fb9d5c42
Janghu <Asheesh.Janghu@tabcorp.com.au>
2021-03-23 18:45:40 +1100

    Inital Commit with just skeleton of project

---
+++ src/test/java/com/swipejobs/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swipejobs.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
